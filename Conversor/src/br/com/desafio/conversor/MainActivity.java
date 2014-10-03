6aec3108a35f36fb58ed411a46c89d8ef26a481e
jvgengo <jvgengo@gmail.com>
2014-10-02 22:34:21 -0300

    criacao do request do get, faltando gerar a url correta

--- Conversor/src/br/com/desafio/conversor/MainActivity.java
+++ Conversor/src/br/com/desafio/conversor/MainActivity.java
@@ -1,19 +1,26 @@
 package br.com.desafio.conversor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.EditText;
 import android.widget.Toast;
+import org.json.*;
 
 public class MainActivity extends Activity implements OnItemSelectedListener {
 
@@ -27,10 +34,14 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 	private Handler handlerAlteraValorConvertido = new Handler();
 	private ArrayAdapter<CharSequence> adapter;
 
+	private static final String MOEDA_BRASIL = "BRL";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		
 		spMoeda = (Spinner) findViewById(R.id.spinner_moedas);
 		
@@ -77,23 +88,56 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 	 * 
 	 * @param valor para conversao da moeda
 	 */
-	private void processarConversao(int valor) {
+	private void processarConversao(int valorParaConversao) {
+		
+		setProgressBarIndeterminateVisibility(true);
+		
 		new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
+				int valorConvertido = 0;
+				try {
+					URL urlRateExchange = new URL("");
+					HttpURLConnection conexaoURL = (HttpURLConnection) urlRateExchange.openConnection();
+					
+					conexaoURL.setRequestProperty("Request-Method", "GET");
+					conexaoURL.setDoInput(true);  
+					conexaoURL.setDoOutput(true); 
+					
+					conexaoURL.connect();
+					
+					BufferedReader brValor = new BufferedReader(new InputStreamReader(conexaoURL.getInputStream())); 
+					String stringJson = "";
+					
+					if (brValor.ready()) {
+						stringJson = brValor.readLine();
+					}
+					
+					JSONObject valorJSon = new JSONObject(stringJson);
+					
+					valorConvertido = (Integer) valorJSon.get("rate");
+					
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 				
-				final int valorConvertido = 0;
 				
-				//requisicao get aqui
+				final String novoTextoValorConvertido = valorConvertido+"";
 				
 				handlerAlteraValorConvertido.post(new Runnable() {
 					
 					@Override
 					public void run() {
 						
-						txtValorConvertido.setText(String.valueOf(valorConvertido));
+						txtValorConvertido.setText(novoTextoValorConvertido);
 						btnConverter.setEnabled(true);
+						
+						setProgressBarIndeterminateVisibility(false);
 						
 					}
 				});
@@ -101,5 +145,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener {
 			}
 		}).start();
 	}
+	
+	
 
 }
